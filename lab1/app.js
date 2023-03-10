82ed5669698a555532ccf19b7acd6220c875cb1c
chaungocchuong <chuongcnpd07596@fpt.edu.vn>
2023-03-10 10:36:18 +0700

    sua bai tap lab1

---
+++ lab1/app.js
@@ -0,0 +1,7 @@
+let a;// khai báo biến a
+a=parseInt(prompt("moi nahp vao a"));
+let b;// khai báo biến b
+b=parseInt(prompt("moi nhap vao b"));
+let c= a+b; // khai báo và gán cho biết c = a+b
+console.log(`ket qua cua phep tinh ${a}+${b}=${c}`); // hiển thị kết quả trong phần console
+document.write(`ket qua cua phep tinh ${a}+${b}=${c}`); // hiển thị kết quả trên trang web
