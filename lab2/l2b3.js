7496cd9dd14c420d08f73bc4e287ca30c485fc1e
chaungocchuong <chuongcnpd07596@fpt.edu.vn>
2023-03-23 21:48:16 +0700

    noplab

---
+++ lab2/l2b3.js
@@ -0,0 +1,9 @@
+var hoten;
+hoten = tostring(prompt("moi ban nhap vao ho ten: "));
+var gt;
+gt= toString(prompt("moi ban nhap vao gioi tinh:"));
+tach = hoten.split(" ");
+if (gt= "nam")
+{
+    tach.splice(1,1, "van");
+}
